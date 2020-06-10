ccda2ffac13676bf637f4fb66ce3904d0801c2e3
Nailton Sousa <nailton.osn@gmail.com>
2020-06-09 21:47:19 -0300

    init dummyTransactions

--- script.js
+++ script.js
@@ -4,4 +4,29 @@
  * @date    2020-06-09 21:19:43
  * @version $Id$
  */
+const transactionUl = document.querySelector('#transactions')
 
+const dummyTransactions = [
+    { id: 1, name: 'Bolo de cenoura', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'Torta de frango', amount: -10 },
+    { id: 3, name: 'Violão', amount: 150 }
+]
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const cssClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+li.classList.add(cssClass)
+li.innerHTML = `
+    ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+`
+transactionUl.append(li)
+}
+
+const init = () => {
+  dummyTransactions.forEach(addTransactionIntoDOM)
+}
+init()
