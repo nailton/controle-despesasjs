024cdf6670b9d7cdd6fced93caf58c16760c882d
Nailton Sousa <nailton.osn@gmail.com>
2020-06-10 10:41:52 -0300

    Display in DOM "saldo", "receitas" and "despesas"

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
  * @version $Id$
  */
 const transactionUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     { id: 1, name: 'Bolo de cenoura', amount: -20 },
@@ -19,21 +22,38 @@ const addTransactionIntoDOM = transaction => {
     const amountWithoutOperator = Math.abs(transaction.amount)
     const li = document.createElement('li')
 
-li.classList.add(cssClass)
-li.innerHTML = `
+    li.classList.add(cssClass)
+    li.innerHTML = `
     ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
 `
-transactionUl.append(li)
+    transactionUl.append(li)
 }
 
 const updateBalanceValues = () => {
-  const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
-  const total = transactionsAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
-  console.log(total)
+    const transactionsAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+
+    const expense = Math.abs(transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0))
+        .toFixed(2)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
 }
 
 const init = () => {
-  dummyTransactions.forEach(addTransactionIntoDOM)
-  updateBalanceValues()
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
 }
 init()
